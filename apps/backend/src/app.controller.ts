091f7bee3473e36029ac633c984b9cbce0089c5a
Kamil Zając <kamil@dunefront.com>
2025-07-31 23:32:01 +0200

    livecheck endpoint

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,5 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+
+    @Get()
+    liveCheck() {
+        return 'OK';
+    }
 }
